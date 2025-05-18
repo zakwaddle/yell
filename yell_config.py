f73766d0a68498dca6151a5f0555505f8e8ed614
Zak Waddle <zakwaddle@gmail.com>
2025-05-18 01:11:10 -0400

    Add initial yell_config.py with default configuration
    This commit introduces a new configuration file, yell_config.py, containing default settings for customization and module control. The configuration includes theme usage, text formatting options, and initial module definitions. This sets the foundation for future configurable behavior.

---
+++ yell_config.py
@@ -0,0 +1,17 @@
+config = {
+    "use_theme": True,
+    "all_quiet": False,
+    "truncate": False,
+    "wrap": True,
+    "width": 120,
+
+    "custom_class_a": None,
+    "custom_color_a": "bright_magenta",
+
+    "modules": {
+        # ----- [App] -----
+        "test": {
+            "lvl": 1, "on": True
+        },
+    }
+}
