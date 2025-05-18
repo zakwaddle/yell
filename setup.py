bd51233d89828ebe4b81ef0ff8ec7d16dc31f506
Zak Waddle <zakwaddle@gmail.com>
2025-05-18 01:11:25 -0400

    Update package version to 0.1.1
    Incremented the package version from 0.1.0 to 0.1.1 to reflect new changes or fixes. This ensures compatibility and clarity for downstream users or dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='yell',
-    version='0.1.0',
+    version='0.1.1',
     author='Zak',
     author_email='zakwaddle@gmail.com',
     description="A stylized developer console logger with themes and introspection.",
