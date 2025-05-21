ac9654d06ba03336056367084ae50f9fee8e7d62
Zak Waddle <zakwaddle@gmail.com>
2025-05-20 22:51:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name='yell',
-    version='0.1.1',
-    author='Zak',
-    author_email='zakwaddle@gmail.com',
-    description="A stylized developer console logger with themes and introspection.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    version='0.1.2',
+    url='https://github.com/zakwaddle/yell',
     packages=find_packages(where="."),
     package_dir={"": "."},
+    description="A stylized developer console logger with themes and introspection.",
+    author='Zak',
+    author_email='zakwaddle@gmail.com',
     install_requires=[],
     python_requires=">=3.7",
 )
