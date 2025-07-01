f392daec04927b9ccf1c2f534adf55d84de4773b
Zak Waddle <zakwaddle@gmail.com>
2025-07-01 15:49:46 -0400

    Update version to 0.1.4 in setup.py
    This version bump prepares the package for a new release. Ensure dependencies and environments reflect the updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yell',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/zakwaddle/yell',
     packages=find_packages(where="."),
     package_dir={"": "."},
