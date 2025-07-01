86e862fc001f7602c85b022b50882c9cd99145f2
Zak Waddle <zakwaddle@gmail.com>
2025-07-01 15:59:29 -0400

    Bump version to 0.1.5
    Update package version from 0.1.4 to 0.1.5 in setup.py. This prepares the project for a new release with potential updates or fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yell',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/zakwaddle/yell',
     packages=find_packages(where="."),
     package_dir={"": "."},
